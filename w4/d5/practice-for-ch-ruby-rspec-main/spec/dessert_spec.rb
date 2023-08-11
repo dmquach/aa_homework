30a53b891d9d91f3aee44510a6d7d34786e22b03
Dylan <dylanquach@gmail.com>
2023-08-11 10:08:22 -0700

    Finish w4d5

--- w4/d5/practice-for-ch-ruby-rspec-main/spec/dessert_spec.rb
+++ w4/d5/practice-for-ch-ruby-rspec-main/spec/dessert_spec.rb
@@ -35,22 +35,42 @@ describe Dessert do
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array" do
-      
+  it "shuffles the ingredient array" do
+    ingredients = ["chocolate", "flour", "egg", "sugar", "butter"]
+
+    ingredients.each do |ingredient|
+      dessert.add_ingredient(ingredient)
+    end
+
+    expect(dessert.ingredients).to eq(ingredients)
+    dessert.mix!
+    expect(dessert.ingredients).not_to eq(ingredients)
+    expect(dessert.ingredients.sort).to eq(ingredients.sort)
+  end
+end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      dessert.eat(1)
+      expect(dessert.quantity).to eq(9)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect { dessert.eat(120)}.to raise_error("not enough left!")
     end
   end
 
-  describe "#eat" do
-    it "subtracts an amount from the quantity"
-
-    it "raises an error if the amount is greater than the quantity"
-  end
-
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef")
+      expect(dessert.serve).to eq("Chef has made 10 desserts!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(dessert)
+      dessert.make_more
+    end
   end
 end
