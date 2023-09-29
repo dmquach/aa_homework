ce0c3846488e89c3b7d6706a5b0e2a57c073a173
Dylan <dylanquach@gmail.com>
2023-09-29 08:53:02 -0700

    Add w11d5

---
+++ w11/d4/practice-for-ch-react-jbuilder-long-practice/app/views/api/gifts/index.json.jbuilder
@@ -0,0 +1,5 @@
+json.array! @gifts do |gift|
+  json.partial! "api/gifts/gift", gift: gift
+  json.party gift.party.name
+end
+
---
+++ w11/d4/practice-for-ch-react-jbuilder-long-practice/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    resources :guests, only: [:index, :show] do
+      resources :gifts, only: [:index, :show], shallow: true 
+    end
+    resources :parties, only: [:index, :show]
+  end
+end
